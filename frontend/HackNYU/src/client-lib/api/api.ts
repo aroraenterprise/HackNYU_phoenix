504f8616739281a7a31d2e5140653168c49a7ddc
Saj Arora <saj.arora24@gmail.com>
2017-10-27 01:27:13 -0400

    adding client lib to frontend

--- frontend/HackNYU/src/client-lib/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './account.service';
-import { AccountService } from './account.service';
-export * from './app.service';
-import { AppService } from './app.service';
-export const APIS = [AccountService, AppService];
